5318003e3bd3b0f063db95de798cf4f528f50c81
.email.com <gangisettymani98@gmail.com>
2026-04-10 11:54:50 +0530

    i have create by new variable

---
+++ 06-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+a=3
+b=5
+if ["$a" -ge "$b"]
+then
+echo "the variable statement to fine 3 greater than 5 other"
+else
+echo "the variable statement to fine 5 greater than 3 other"
+fi
