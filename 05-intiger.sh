0182d2feaa771a5c92040084c127be018fab5e6d
.email.com <gangisettymani98@gmail.com>
2026-04-10 11:40:24 +0530

    i have modified another new file

--- 05-intiger.sh
+++ 05-intiger.sh
@@ -5,9 +5,9 @@ b=3
 
 if ["$a" -eq "$b"]
 then
-echo "two both varible the same"
+echo " both varible are the same"
 else
-echo "two both varible the different"
+echo " both varible are the different"
 fi
 
 
